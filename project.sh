21db621a0d1631e4ef4242999e524fec3d00cf01
Vishal <vishal.auroville@gmail.com>
2024-06-09 11:50:44 +0530

    first_commit

---
+++ project.sh
@@ -0,0 +1,7 @@
+# URL of the YouTube video
+URL="https://www.youtube.com/watch?v=Q6mcDZui71I"
+
+# Open the URL in the default browser
+xdg-open $URL
+
+#chatgpt blog https://chatgpt.com/share/9d43db44-bdf8-448c-9283-475dd5c7a609
